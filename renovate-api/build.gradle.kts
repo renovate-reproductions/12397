82e7537ee4a6ace7646cc84ae4449065f9cff8cf
huisam <huisam@naver.com>
2021-10-29 21:19:02 +0900

    chore: add mockk library for test

--- renovate-api/build.gradle.kts
+++ renovate-api/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(Dependencies.mockk)
 }
