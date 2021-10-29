632769a5309880c60a6b8baa7405ba8dc1a03854
huisam <huisam@naver.com>
2021-10-29 20:42:10 +0900

    chore: add mockk dependency version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,4 +6,8 @@ object Dependencies {
     // Kotlin
     const val kotlinStdlibJdk8Extension = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
     const val kotlinReflection = "org.jetbrains.kotlin:kotlin-reflect"
+    
+    // mockk
+    private const val mockkVersion = "1.11.0"
+    const val mockk = "io.mockk:mockk:$mockkVersion"
 }
